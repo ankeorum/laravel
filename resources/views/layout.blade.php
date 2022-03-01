b317c311468250284e2d8b1321250f73f5750166
ankeorum <ankeorum@gmail.com>
2022-03-01 08:31:04 +0100

    commit curso leccion 18

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -4,16 +4,15 @@
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<title>@yield('title')</title>
+	<style type="text/css">
+		.active a {
+			color: red;
+			text-decoration: none;
+		}
+	</style>
 </head>
 <body>
-	<nav>
-		<ul>
-			<li><a href="/">Home</a></li>
-			<li><a href="/about">About</a></li>
-			<li><a href="/portfolio">Portfolio</a></li>
-			<li><a href="/contact">Contact</a></li>
-		</ul>
-	</nav>
+	@include('partials.nav')
 	@yield('content')
 </body>
 </html>
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PortfolioController;
+use App\Http\Controllers\HomeController;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -13,13 +16,14 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-
-Route::view('/','home')->name('home');
+//Route::view('/', 'home')->name('home');
+Route::get('/',HomeController::class)->name('home');
 Route::view('/about','about')->name('about');
 Route::view('/contact','contact')->name('contact');
 
-Route::get('/portfolio', 'PortfolioController');
+Route::get('/portfolio', [PortfolioController::class, 'index'])->name('portfolio');
 
+// Route::apiresource('projects',PortfolioController::class);
 // Route::get('/', function () {
 //     $nombre = "Gonzalo";
 //     return view('home', compact('nombre'));
